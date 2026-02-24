7a55fe67966566c6be8f8b584bf949e595caeb64
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2026-02-24 04:32:57 +0000

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import profile2 from '../assets/profile2.png'
 export function About() {
   return (
     <section id="about" className="py-16 bg-white dark:bg-gray-950">
@@ -16,7 +17,7 @@ export function About() {
               <div className="absolute inset-0 bg-gradient-to-t from-black/30 via-transparent to-transparent mix-blend-multiply pointer-events-none" />
 
               <img
-                src="assets/profile2.png"
+                src={profile2}
                 alt="Portrait of Fatima Zahra Zaha"
                 className="w-full h-full object-cover rounded-[2rem]"
               />
